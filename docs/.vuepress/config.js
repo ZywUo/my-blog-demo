ba8a5f705dd9d72cc7b67d49590cab7867098ebd
Zhisheng Tao <taozhisheng23@mails.ucas.ac.cn>
2025-09-16 12:14:10 +0800

    change for depoly by vercel

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base: '/my-blog-demo/',
+  base: '/',
   lang: 'zh-CN',
 
   title: '博客',
